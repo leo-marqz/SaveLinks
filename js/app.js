fff7e9513556dac5cb456fa9c404ccda4b1455c2
Leonel Marquez <leomarqz2020@gmail.com>
2021-03-21 15:18:08 -0600

    cambiando apariencia de la inerfaz

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-
+document.addEventListener("DOMContentLoaded", ()=>{
+  Swal.fire({
+    text:'Bienvenido a SaveLinks, aqui podras guardar tus enlaces mas importantes solo debes tener cuidado cuando borres datos del navegador ya que tus enlaces se guardaran en la memoria de este.',
+    icon: 'info',
+    confirmButtonText: 'Entendido'
+  });
+})
 
 // main containers
 let links = document.querySelector('#links');
@@ -7,40 +13,45 @@ let form = document.querySelector('#main');
 
 let title = document.querySelector('#title');
 let url = document.querySelector('#url');
-let description = document.querySelector('#description');
 
 let btnSaveLinks = document.querySelector('#btn-saveLinks');
 btnSaveLinks.addEventListener('click',saveLink);
-// console.log(btnSaveLinks)
 
 function saveLink(e) {
   
   let t = title.value;
   let u = url.value;
-  let d = description.value;
 
-  if(t == "" && u == "" && d == ""){ 
-    alert('you must enter data such as a title, a description and a valid link.');
+  if(t == "" && u == ""){ 
+    Swal.fire({
+      title: 'Debes datos en ambos campos',
+      icon: 'warning',
+      confirmButtonText: 'vale'
+    });
   }else{
     const datalinks = {
       title:t,
-      description:d,
       url:u,
     }
   
-    if(localStorage.getItem('links') === null){
+    if(localStorage.getItem('myLinks') === null){
       let links = [];
       links.push(datalinks);
       // console.log(links);
-      localStorage.setItem('links',JSON.stringify(links));
+      localStorage.setItem('myLinks',JSON.stringify(links));
     }else{
-      let links = JSON.parse(localStorage.getItem('links'));
+      let links = JSON.parse(localStorage.getItem('myLinks'));
       links.push(datalinks);
-      localStorage.setItem('links',JSON.stringify(links));
+      localStorage.setItem('myLinks',JSON.stringify(links));
     }
   
     clearForm();
     getLinks();
+    Swal.fire({
+      title: 'Enlace Guardado',
+      icon: 'success',
+      confirmButtonText: 'ok'
+    })
   }
 
   e.preventDefault();
@@ -50,49 +61,49 @@ function saveLink(e) {
 getLinks();
 
 function getLinks(){
-  let data = JSON.parse(localStorage.getItem('links'));
+  let data = JSON.parse(localStorage.getItem('myLinks'));
   let spacingLinks = document.querySelector('#links');
   spacingLinks.innerHTML = '';
 
   for(let i = 0; i < data.length; i++){
     let _title = data[i].title;
     let _url = data[i].url;
-    let _description = data[i].description;
     spacingLinks.innerHTML += `
     <tr>
-        <th class="p-1 text-center align-middle">${i + 1}</th>
-        <th class="p-1 fw-normal align-middle">${_title}</th>
-        <th class="p-1 text-secondary fw-light align-middle">${_description}</th>
-        <th class="p-1 text-primary fw-light align-middle">${_url}</th>
-        <th class="p-1">
+        <td class="p-1 text-center align-middle">${i + 1}</td>
+        <td class="p-1 fw-normal align-middle">${_title}</td>
+        <td class="p-1">
           <a class="btn btn-success" href="${_url}" target="_blank" >Open Link</a>
           <a class="btn btn-primary" onclick="copyLink('${_url}')">Copy Link</a>
           <a class="btn btn-danger" onclick="DeleteLink('${_title}')" >Delete</a>
-        </th>
+        </td>
      </tr>`;
   }
 }
 
 function DeleteLink(title) { 
-  let data = JSON.parse(localStorage.getItem('links'));
+  let data = JSON.parse(localStorage.getItem('myLinks'));
   for(let i = 0; i < data.length; i++){
     if(data[i].title == title){
       data.splice(i, 1);
     }
   }
-  localStorage.setItem('links', JSON.stringify(data));
+  localStorage.setItem('myLinks', JSON.stringify(data));
   getLinks();
+  Swal.fire({
+    title: 'Enlace Eliminado',
+    icon: 'success',
+    confirmButtonText: 'ok'
+  })
  }
 
 
 function clearForm() { 
   let title = document.getElementById('title');
   let url = document.getElementById('url');
-  let desc = document.getElementById('description');
 
   title.value = "";
   url.value = "";
-  desc.value = "";
  }
 
 function copyLink(url) {
@@ -103,6 +114,11 @@ function copyLink(url) {
   document.execCommand('copy');
   console.log('copiado');
   document.body.removeChild(inputFalse);
+  Swal.fire({
+    title: 'Enlace Copiado',
+    icon: 'success',
+    confirmButtonText: 'ok'
+  });
 }
 
 
